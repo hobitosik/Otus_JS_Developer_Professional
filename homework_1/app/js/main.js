d0b26b1f5d285f35453d6b874a784337ddb2dbbb
Elli <dlyaverovna@yandex.com>
2020-10-14 01:30:47 +0300

    Доработки

--- homework_1/app/js/main.js
+++ homework_1/app/js/main.js
@@ -1,29 +1,26 @@
 'use strict';
 
-const usersItemsArr = [["a", "b"], ["a", "c"], ["d", "e"]];
-
 function maxItemAssociation( usersItems ){
-    let result = [];
     let allItems = usersItems.reduce(( acc, items )=>{
         items.forEach( item => { 
-            if( !acc.includes( item ) ) 
+            if( !acc.includes( item ) )
                 acc.push( item ) 
         })
         return acc
     }, []);
-    
-    let groupsArr = getСrossingArr( usersItems, allItems );
-    result = sortLengthLexic( groupsArr )[0].split('')
-    console.log('maxItemAssociation:', result)
+    let groupsArr = getСrossingArr( usersItems, allItems )
+    let result = sortLengthLexic( groupsArr )[0].split('')
+    console.log('maxItemAssociation:', result);
+    alert(`maxItemAssociation: ${result}`);
     return result
 }
 
 function sortLengthLexic( arr ){
     let itemLexicArr = arr.map( itemArr => itemArr.sort().join('') ).sort();
-    return itemLexicArr.sort((a, b) => a.length <= b.length ? 1 : -1);
+    return itemLexicArr.sort(( a, b ) => a.length <= b.length ? 1 : -1 );
 }
 
-function getСrossingArr( usersItems, allItems ){ // [["a", "b"], ["a", "c"], ["d", "e"]]
+function getСrossingArr( usersItems, allItems ){
     let crossingArr = allItems.reduce(( acum, itm ) => {
 
         let union = usersItems.reduce(( acc, counter )=>{
@@ -38,7 +35,8 @@ function getСrossingArr( usersItems, allItems ){ // [["a", "b"], ["a", "c"], ["
             })
             return acc
         }, []);
-        if( isIncludesToArr( acum, joinUnionArr ) )
+
+        if( isIncludesToArr( acum, joinUnionArr ) && !isContainsToArr( acum, joinUnionArr ) )
             acum.push( joinUnionArr )
 
         return acum
@@ -50,6 +48,11 @@ function getСrossingArr( usersItems, allItems ){ // [["a", "b"], ["a", "c"], ["
 
 function isIncludesToArr( arr, addedArr ){
     let joinArr = arr.map( item => item.sort().join(''))
-    return joinArr.indexOf( addedArr.sort().join('') ) === -1
+    let joinAddedArr = addedArr.sort().join('')
+    return joinArr.indexOf( joinAddedArr ) === -1
 }
 
+function isContainsToArr( arr, addedArr ){
+    let joinArr = arr.map( item => item.sort().join(''))
+    return addedArr.some( item => joinArr.some( str => ~str.indexOf( item ) ))
+}
